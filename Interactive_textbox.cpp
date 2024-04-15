060fd0951e528111647fe5dc4a1f51e70dd958e8
Vanisch <staniszfabian@gmail.com>
2024-04-15 09:56:46 +0200

    Commit 2

--- Interactive_textbox.cpp
+++ Interactive_textbox.cpp
@@ -1,5 +1,39 @@
 ﻿#include <iostream>
-int main()
-{
-    std::cout << "Hello World!\n";
+#include <Windows.h>
+#include <conio.h>
+#include <thread> 
+#include <chrono>
+
+
+using namespace std::chrono_literals;
+using namespace std;
+
+HANDLE console;
+
+void Draw() {
+    COORD pos;
+    pos.X = 30;
+    pos.Y = 10;
+
+
+    SetConsoleTextAttribute(console, BACKGROUND_BLUE);
+
+    for (int i = 0; i < 32; ++i) {
+        SetConsoleCursorPosition(console, pos);
+        std::cout << " ";
+        pos.X++;
+    }
+}
+void Write() {
+
+}
+
+int main() {
+    console = GetStdHandle(STD_OUTPUT_HANDLE);
+    Draw();
+    COORD pos;
+    pos.X = 30;
+    pos.Y = 10;
+    SetConsoleCursorPosition(console, pos);
+    cout << "Test";
 }
