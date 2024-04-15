e01057831899e147e616ee090c1a6e4965bd12ce
Vanisch <staniszfabian@gmail.com>
2024-04-15 20:30:51 +0200

    Commit 2

--- Interactive_textbox.cpp
+++ Interactive_textbox.cpp
@@ -16,7 +16,7 @@ void Draw() {
     pos.Y = 10;
 
 
-    SetConsoleTextAttribute(console, BACKGROUND_BLUE);
+    SetConsoleTextAttribute(console, BACKGROUND_GREEN);
 
     for (int i = 0; i < 32; ++i) {
         SetConsoleCursorPosition(console, pos);
@@ -25,15 +25,28 @@ void Draw() {
     }
 }
 void Write() {
-
+    COORD pos;
+    pos.X = 30;
+    pos.Y = 10;
+    SetConsoleCursorPosition(console, pos);
+    int input{ 0 };
+    while (input != 27)
+    {
+        if (_kbhit())
+        {
+            input = _getch();
+            if (pos.X <= 61 && input!=8 && 32<=input) {
+                pos.X++;
+                cout << input;
+            }
+            
+        }
+    }
 }
 
 int main() {
     console = GetStdHandle(STD_OUTPUT_HANDLE);
     Draw();
-    COORD pos;
-    pos.X = 30;
-    pos.Y = 10;
-    SetConsoleCursorPosition(console, pos);
-    cout << "Test";
+    Write();
 }
+
