2ff6b81f1de146df11fd1744ab248b619a26206f
Vanisch <staniszfabian@gmail.com>
2024-04-16 15:54:50 +0200

    Final Version

--- Interactive_textbox.cpp
+++ Interactive_textbox.cpp
@@ -11,43 +11,39 @@ using namespace std;
 
 HANDLE console;
 
+//Function drawing green square.
+
 void Draw() {
-    COORD pos;
+    COORD pos;                                              //Setting position of square.
     pos.X = 30;
     pos.Y = 10;
 
 
-    SetConsoleTextAttribute(console, BACKGROUND_GREEN);
+    SetConsoleTextAttribute(console, BACKGROUND_GREEN);     //Changing background to green color.
 
-    for (int i = 0; i < 32; ++i) {
+    for (int i = 0; i < 32; ++i) {                          //Drwaing square.
         SetConsoleCursorPosition(console, pos);
         std::cout << " ";
         pos.X++;
     }
 }
+//Function for entering and deleting characters and changing the cursor position.
 void Write() {
-    COORD pos;
+    COORD pos;                                              //Setting positon of cursor.
     pos.X = 30;
     pos.Y = 10;
     SetConsoleCursorPosition(console, pos);
     int input = 0;
-    while (input != 27)
+    while (input != 27)                                     //Loop to turn off program when ESC pressed.
     {
         if (_kbhit())
         {
             input = _getch();
-            if (input == 224 || input == 0) {
-                switch (_getch()) {
-                case 8:
-                    if (pos.X > 30) {
-                        pos.X -= 1;
-                    }
-                    SetConsoleCursorPosition(console, pos);
-                    cout << char(0);
-                    break;
+            if ((input == 224 || input == 0) && _kbhit()==1) {      //Catching arrow keys and function keys to move cursor around. (Arrows on numpad works to, when number lock is on.)
+                switch (_getch()) {                                 
                 case 75:
                     if (pos.X > 30) {
-                        pos.X-=1;
+                        pos.X -= 1;
                     }
                     SetConsoleCursorPosition(console, pos);
                     break;
@@ -59,16 +55,33 @@ void Write() {
                     break;
                 }
             }
-            else {
-                if (pos.X <= 61 && 30<=pos.X && 32 <= input && input <= 255) {
+            else {                                                                      //Printing characters.
+                if ((pos.X <= 61 && 30 <= pos.X) && (32 <= input && input <= 255)) {
                     pos.X++;
                     cout << char(input);
                 }
+                if ((pos.X <= 62 && 31 <= pos.X) && input == 8) {                         //Making backspace work.
+                    pos.X--;
+                    SetConsoleCursorPosition(console, pos);
+                    cout << char(32);
+                    SetConsoleCursorPosition(console, pos);
+                }
+
+                if ((pos.X <= 61 && 30 <= pos.X) && input == 9) {                         //Making TAB work.
+                    cout << char(9);
+                    pos.X += 4;
+                    SetConsoleCursorPosition(console, pos);
+                }
+                if (pos.X > 61 && input == 9) {
+                        pos.X=62;
+                    SetConsoleCursorPosition(console, pos);
+                }
+            }
+            
             }
         }
     }
-}
-
+//Main function.
 int main() {
     console = GetStdHandle(STD_OUTPUT_HANDLE);
     Draw();
