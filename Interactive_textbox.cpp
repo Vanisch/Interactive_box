8996847874ce332b6190a929230ca0c7744d85f5
Vanisch <staniszfabian@gmail.com>
2024-04-16 15:57:23 +0200

    QFix comments

--- Interactive_textbox.cpp
+++ Interactive_textbox.cpp
@@ -14,14 +14,14 @@ HANDLE console;
 //Function drawing green square.
 
 void Draw() {
-    COORD pos;                                              //Setting position of square.
+    COORD pos;                                                                              //Setting position of square.
     pos.X = 30;
     pos.Y = 10;
 
 
-    SetConsoleTextAttribute(console, BACKGROUND_GREEN);     //Changing background to green color.
+    SetConsoleTextAttribute(console, BACKGROUND_GREEN);                                     //Changing background to green color.
 
-    for (int i = 0; i < 32; ++i) {                          //Drwaing square.
+    for (int i = 0; i < 32; ++i) {                                                          //Drwaing square.
         SetConsoleCursorPosition(console, pos);
         std::cout << " ";
         pos.X++;
@@ -29,17 +29,17 @@ void Draw() {
 }
 //Function for entering and deleting characters and changing the cursor position.
 void Write() {
-    COORD pos;                                              //Setting positon of cursor.
+    COORD pos;                                                                              //Setting positon of cursor.
     pos.X = 30;
     pos.Y = 10;
     SetConsoleCursorPosition(console, pos);
     int input = 0;
-    while (input != 27)                                     //Loop to turn off program when ESC pressed.
+    while (input != 27)                                                                     //Loop to turn off program when ESC pressed.
     {
         if (_kbhit())
         {
             input = _getch();
-            if ((input == 224 || input == 0) && _kbhit()==1) {      //Catching arrow keys and function keys to move cursor around. (Arrows on numpad works to, when number lock is on.)
+            if ((input == 224 || input == 0) && _kbhit()==1) {                              //Catching arrow keys and function keys to move cursor around. (Arrows on numpad works to, when number lock is on.)
                 switch (_getch()) {                                 
                 case 75:
                     if (pos.X > 30) {
@@ -55,19 +55,19 @@ void Write() {
                     break;
                 }
             }
-            else {                                                                      //Printing characters.
+            else {                                                                          //Printing characters.
                 if ((pos.X <= 61 && 30 <= pos.X) && (32 <= input && input <= 255)) {
                     pos.X++;
                     cout << char(input);
                 }
-                if ((pos.X <= 62 && 31 <= pos.X) && input == 8) {                         //Making backspace work.
+                if ((pos.X <= 62 && 31 <= pos.X) && input == 8) {                           //Making backspace work.
                     pos.X--;
                     SetConsoleCursorPosition(console, pos);
                     cout << char(32);
                     SetConsoleCursorPosition(console, pos);
                 }
 
-                if ((pos.X <= 61 && 30 <= pos.X) && input == 9) {                         //Making TAB work.
+                if ((pos.X <= 61 && 30 <= pos.X) && input == 9) {                           //Making TAB work.
                     cout << char(9);
                     pos.X += 4;
                     SetConsoleCursorPosition(console, pos);
